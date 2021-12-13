8f675cc6778f9441f1ce377e0a8d1e3c0e21a8a0
charles.chengc <charles.chengc@alibaba-inc.com>
2021-12-13 17:06:03 +0800

    modify application name

--- copy.sh
+++ copy.sh
@@ -6,7 +6,10 @@ rm -f copy.sh
 rm -rf .* 
 rm -rf target
 grep -rl "baseweb" --include="pom.xml" ./|xargs sed -i "" "s/baseweb/$1/g"
+grep -rl "baseweb" --include="MyApplication.java" ./|xargs sed -i "" "s/MyApplication/My_$1_Application/g"
 cd src
 grep -rl "baseweb" --include="*" ./|xargs sed -i "" "s/baseweb/$1/g"
 cd main/java/
 mv baseweb $1
+cd $1
+mv MyApplication.java My_$1_Application.java
