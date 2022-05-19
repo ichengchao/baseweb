9856180e0c4e31aff7cca1e0b3cd1b3b62356baa
程超 <charles.chengc@alibaba-inc.com>
2022-05-19 10:35:51 +0800

    fix copy on linux env

--- copy.sh
+++ copy.sh
@@ -2,13 +2,20 @@
 cd ..
 cp -r baseweb $1
 cd $1
-rm -f copy.sh
-rm -rf .* 
-rm -rf target
-grep -rl "baseweb" --include="pom.xml" ./|xargs sed -i "" "s/baseweb/$1/g"
-grep -rl "baseweb" --include="MyApplication.java" ./|xargs sed -i "" "s/MyApplication/My_$1_Application/g"
-cd src
-grep -rl "baseweb" --include="*" ./|xargs sed -i "" "s/baseweb/$1/g"
+rm -rf copy.sh target .* 
+if [ "$(uname)" = "Darwin" ]; then
+    echo "mac"
+    grep -rl "baseweb" --include="pom.xml" ./|xargs sed -i "" "s/baseweb/$1/g"
+    grep -rl "baseweb" --include="MyApplication.java" ./|xargs sed -i "" "s/MyApplication/My_$1_Application/g"
+    cd src
+    grep -rl "baseweb" --include="*" ./|xargs sed -i "" "s/baseweb/$1/g" 
+elif [ "$(uname)" = "Linux" ]; then
+    echo "linux"
+    grep -rl "baseweb" --include="pom.xml" ./|xargs sed -i "s/baseweb/$1/g"
+    grep -rl "baseweb" --include="MyApplication.java" ./|xargs sed -i "s/MyApplication/My_$1_Application/g"
+    cd src
+    grep -rl "baseweb" --include="*" ./|xargs sed -i "s/baseweb/$1/g"
+fi
 cd main/java/
 mv baseweb $1
 cd $1
