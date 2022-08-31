58ec2a927ee316a0bf5555212f9c75c0a1b162cf
程超 <charles.chengc@alibaba-inc.com>
2022-08-31 11:37:03 +0800

    add controller aspect

--- src/main/java/baseweb/MyBaseWebApplication.java
+++ src/main/java/baseweb/MyBaseWebApplication.java
@@ -3,8 +3,10 @@ package baseweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @ServletComponentScan("baseweb")
 
 public class MyBaseWebApplication {
