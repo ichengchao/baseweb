1d4613e3c4b36ee32857afa61da5b4102d4e89fa
charles <kidccc@gmail.com>
2025-07-09 17:46:25 +0800

    Revert "feat: Rewrite grid page with Bootstrap and add to menu"
    This reverts commit 9d83b6cfe99382ba3144f525199a6008823b16c2.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -16,11 +16,6 @@ Ext.onReady(function () {
                             href: 'demo/grid.html',
                             hrefTarget: 'mainFrame'
                         }, {
-                            text: "Bootstrap Grid",
-                            leaf: true,
-                            href: 'demo/newgrid.html',
-                            hrefTarget: 'mainFrame'
-                        }, {
                             text: "grid paging",
                             leaf: true,
                             href: 'demo/grid_paging.html',
